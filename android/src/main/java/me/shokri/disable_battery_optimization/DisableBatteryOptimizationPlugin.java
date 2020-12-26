c256218ee80213948a116675b88376f356df748f
Mehrdad Shokri <code.poet95@gmail.com>
2020-12-26 12:57:55 +0330

    Implement battery optimization methods

--- android/src/main/java/me/shokri/disable_battery_optimization/DisableBatteryOptimizationPlugin.java
+++ android/src/main/java/me/shokri/disable_battery_optimization/DisableBatteryOptimizationPlugin.java
@@ -1,5 +1,7 @@
 package me.shokri.disable_battery_optimization;
 
+import android.os.Build;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -24,16 +26,58 @@ public class DisableBatteryOptimizationPlugin implements FlutterPlugin, MethodCa
   }
 
   @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
-  }
-
-  @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     channel.setMethodCallHandler(null);
   }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    }     else if (call.method.equals("isIgnoringBatteryOptimizations")) {
+      boolean reading = isIgnoringBatteryOptimizations();
+      result.success(reading);
+      return;
+    }
+    else if (call.method.equals("openBatteryOptimizationSettings")) {
+      String reading = openBatteryOptimizationSettings();
+      result.success(reading);
+      return;
+    }
+    else if (call.method.equals("stopOptimizingBatteryUsage")) {
+      String reading = stopOptimizingBatteryUsage();
+      result.success(reading);
+      return;
+    }
+    else {
+      result.notImplemented();
+    }
+  }
+  private boolean isIgnoringBatteryOptimizations() {
+    String packageName = mRegistrar.activeContext().getPackageName();
+    mPowerManager = (PowerManager) (mRegistrar.activeContext().getSystemService(POWER_SERVICE));
+
+    if(mPowerManager.isIgnoringBatteryOptimizations(packageName)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private String openBatteryOptimizationSettings() {
+    Intent intent = new Intent();
+    intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+    mRegistrar.activeContext().startActivity(intent);
+    return "Success";
+  }
+
+  private String stopOptimizingBatteryUsage() {
+    Intent intent = new Intent();
+    String packageName = mRegistrar.activeContext().getPackageName();
+    intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+    intent.setData(Uri.parse("package:" + packageName));
+    mRegistrar.activeContext().startActivity(intent);
+    return "Success";
+  }
+
 }
--- lib/disable_battery_optimization.dart
+++ lib/disable_battery_optimization.dart
@@ -11,4 +11,20 @@ class DisableBatteryOptimization {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  static Future<bool> isIgnoringBatteryOptimizations() async {
+    final reading = await _channel.invokeMethod('isIgnoringBatteryOptimizations');
+    return reading;
+  }
+
+  static Future<String> openBatteryOptimizationSettings() async {
+    final reading = await _channel.invokeMethod('openBatteryOptimizationSettings');
+    return reading;
+  }
+
+  static Future<String> stopOptimizingBatteryUsage() async {
+    final reading = await _channel.invokeMethod('stopOptimizingBatteryUsage');
+    return reading;
+  }
+
 }
